1b1e4718fea6495b86b46b4dad69543204d4f41e
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2024-09-07 23:20:06 +0200

    Starters have their Hidden Abilities

--- include/script_pokemon_util.h
+++ include/script_pokemon_util.h
@@ -2,6 +2,7 @@
 #define GUARD_SCRIPT_POKEMON_UTIL_H
 
 u32 ScriptGiveMon(u16 species, u8 level, u16 item);
+u32 ScriptGiveMonStarter(u16 species, u8 level, u16 item);
 u8 ScriptGiveEgg(u16 species);
 void CreateScriptedWildMon(u16 species, u8 level, u16 item);
 void CreateScriptedDoubleWildMon(u16 species, u8 level, u16 item, u16 species2, u8 level2, u16 item2);
