2651d26bc0f36a047a4cda089addbe7900aa5308
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2024-09-07 23:20:06 +0200

    Starters have their Hidden Abilities

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -878,7 +878,7 @@ static void CB2_GiveStarter(void)
 
     *GetVarPointer(VAR_STARTER_MON) = gSpecialVar_Result;
     starterMon = GetStarterPokemon(gSpecialVar_Result);
-    ScriptGiveMon(starterMon, 5, ITEM_NONE);
+    ScriptGiveMonStarter(starterMon, 5, ITEM_NONE);
     ResetTasks();
     PlayBattleBGM();
     SetMainCallback2(CB2_StartFirstBattle);
