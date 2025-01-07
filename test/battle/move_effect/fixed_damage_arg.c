cce3e0bec7860a7ca4c264c159aa8fec653e3707
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2025-01-07 19:38:35 +0100

    Add Mystery Dungeon moves

--- test/battle/move_effect/fixed_damage_arg.c
+++ test/battle/move_effect/fixed_damage_arg.c
@@ -4,6 +4,9 @@
 ASSUMPTIONS
 {
     ASSUME(GetMoveEffect(MOVE_SONIC_BOOM) == EFFECT_FIXED_DAMAGE_ARG);
+    ASSUME(GetMoveEffect(MOVE_VACUUM_CUT) == EFFECT_FIXED_DAMAGE_ARG);
+    ASSUME(GetMoveType(MOVE_VACUUM_CUT) == TYPE_MYSTERY);
+    ASSUME(GetMoveTarget(MOVE_VACUUM_CUT) == MOVE_TARGET_BOTH);
 }
 
 SINGLE_BATTLE_TEST("Sonic Boom deals fixed damage", s16 damage)
@@ -37,5 +40,22 @@ SINGLE_BATTLE_TEST("Sonic Boom doesn't affect ghost types")
     } SCENE {
         NOT ANIMATION(ANIM_TYPE_MOVE, MOVE_SONIC_BOOM, player);
         MESSAGE("It doesn't affect the opposing Gastly…");
+    }
+}
+
+DOUBLE_BATTLE_TEST("Vacuum-Cut bypasses Wonder Guard")
+{
+    GIVEN {
+        PLAYER(SPECIES_SHEDINJA) { Ability(ABILITY_WONDER_GUARD); }
+        PLAYER(SPECIES_SHEDINJA) { Ability(ABILITY_WONDER_GUARD); }
+        OPPONENT(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(opponentLeft, MOVE_VACUUM_CUT); }
+    } SCENE {
+        HP_BAR(playerLeft);
+        MESSAGE("Shedinja fainted!");
+        HP_BAR(playerRight);
+        MESSAGE("Shedinja fainted!");
     }
 }
