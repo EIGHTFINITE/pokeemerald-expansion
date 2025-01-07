c4d4ae40f8149780910ce2c08131444225721050
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2025-01-07 19:38:35 +0100

    Add Mystery Dungeon moves

--- test/battle/ability/wonder_guard.c
+++ test/battle/ability/wonder_guard.c
@@ -46,3 +46,29 @@ SINGLE_BATTLE_TEST("Wonder Guard does not activate when targeted by moves super 
         MESSAGE("Shedinja fainted!");
     }
 }
+
+DOUBLE_BATTLE_TEST("Mystery type attacks bypass Wonder Guard")
+{
+    u32 move;
+    PARAMETRIZE { move = MOVE_VACUUM_CUT; }
+    PARAMETRIZE { move = MOVE_WIDE_SLASH; }
+    GIVEN {
+        ASSUME(GetMoveType(MOVE_VACUUM_CUT) == TYPE_MYSTERY);
+        ASSUME(GetMoveType(MOVE_WIDE_SLASH) == TYPE_MYSTERY);
+        ASSUME(GetMoveTarget(MOVE_VACUUM_CUT) == TARGET_BOTH);
+        ASSUME(GetMoveTarget(MOVE_WIDE_SLASH) == TARGET_BOTH);
+        PLAYER(SPECIES_SHEDINJA) { Ability(ABILITY_WONDER_GUARD); }
+        PLAYER(SPECIES_SHEDINJA) { Ability(ABILITY_WONDER_GUARD); }
+        OPPONENT(SPECIES_WOBBUFFET);
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(opponentLeft, move); }
+    } SCENE {
+        NONE_OF {
+            ABILITY_POPUP(playerLeft, ABILITY_WONDER_GUARD);
+            ABILITY_POPUP(playerRight, ABILITY_WONDER_GUARD);
+        }
+        MESSAGE("Shedinja fainted!");
+        MESSAGE("Shedinja fainted!");
+    }
+}
