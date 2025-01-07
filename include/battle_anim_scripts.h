b45324fa762e13b405215aa40826a389058b070c
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2025-01-07 19:38:35 +0100

    Add Mystery Dungeon moves

--- include/battle_anim_scripts.h
+++ include/battle_anim_scripts.h
@@ -1004,6 +1004,10 @@ extern const u8 gBattleAnimGeneral_TeraCharge[];
 extern const u8 gBattleAnimGeneral_TeraActivate[];
 extern const u8 gBattleAnimGeneral_SimpleHeal[];
 
+// Mystery Dungeon Moves
+extern const u8 gBattleAnimMove_WideSlash[];
+extern const u8 gBattleAnimMove_VacuumCut[];
+
 // special animations
 extern const u8 gBattleAnimSpecial_LevelUp[];
 extern const u8 gBattleAnimSpecial_SwitchOutPlayerMon[];
