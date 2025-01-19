df620e6b27d023c210335158c62cf0c8fda412b7
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2025-01-19 15:53:43 +0100

    Set move limits to minimum working values

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -179,7 +179,7 @@ enum OtIdMethod
 #define LEVEL_UP_MOVE_LV   0xFE00
 #define LEVEL_UP_MOVE_END  0xFFFF
 
-#define MAX_LEVEL_UP_MOVES       20
+#define MAX_LEVEL_UP_MOVES       34
 
 #define MON_MALE       0x00
 #define MON_FEMALE     0xFE
