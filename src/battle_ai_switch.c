426a23511b6a750ccd0a109f9a8aedcd49556124
cawtds <38510667+cawtds@users.noreply.github.com>
2026-01-04 00:58:31 +0100

    fix battler

--- src/battle_ai_switch.c
+++ src/battle_ai_switch.c
@@ -927,7 +927,7 @@ static bool32 CanUseSuperEffectiveMoveAgainstOpponent(u32 battler, u32 opposingB
 {
     enum Move move;
 
-    if (!IsBattlerAlive(battler))
+    if (!IsBattlerAlive(opposingBattler))
         return FALSE;
 
     for (u32 moveIndex = 0; moveIndex < MAX_MON_MOVES; moveIndex++)
