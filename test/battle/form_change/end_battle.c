c2256e5e59740ecb6d9b128af56501ae7a1f477e
Eduardo Quezada <eduardo602002@gmail.com>
2026-01-14 19:37:12 -0300

    Added tests for fainting form changes (#8912)

--- test/battle/form_change/end_battle.c
+++ test/battle/form_change/end_battle.c
@@ -259,13 +259,13 @@ SINGLE_BATTLE_TEST("Morpeko Hangry reverts to Full Belly Form upon battle end af
 
 SINGLE_BATTLE_TEST("Ogerpon reverts to the correct form upon battle end after terastallizing")
 {
-    u32 species;
-    PARAMETRIZE { species = SPECIES_OGERPON_TEAL; }
-    PARAMETRIZE { species = SPECIES_OGERPON_WELLSPRING; }
-    PARAMETRIZE { species = SPECIES_OGERPON_HEARTHFLAME; }
-    PARAMETRIZE { species = SPECIES_OGERPON_CORNERSTONE; }
+    u32 species, item;
+    PARAMETRIZE { species = SPECIES_OGERPON_TEAL;        item = ITEM_NONE; }
+    PARAMETRIZE { species = SPECIES_OGERPON_WELLSPRING;  item = ITEM_WELLSPRING_MASK; }
+    PARAMETRIZE { species = SPECIES_OGERPON_HEARTHFLAME; item = ITEM_HEARTHFLAME_MASK; }
+    PARAMETRIZE { species = SPECIES_OGERPON_CORNERSTONE; item = ITEM_CORNERSTONE_MASK; }
     GIVEN {
-        PLAYER(species);
+        PLAYER(species) { Item(item); }
         OPPONENT(SPECIES_WOBBUFFET);
     } WHEN {
         TURN { MOVE(player, MOVE_CELEBRATE, gimmick: GIMMICK_TERA); }
--- test/battle/move_effect/ivy_cudgel.c
+++ test/battle/move_effect/ivy_cudgel.c
@@ -8,14 +8,12 @@ ASSUMPTIONS
 
 SINGLE_BATTLE_TEST("Ivy Cudgel changes the move type depending on the form of Ogerpon")
 {
-    u16 species;
-    u16 ogerpon;
-    u16 item;
+    u32 species, ogerpon, item;
 
-    PARAMETRIZE { species = SPECIES_BLASTOISE; ogerpon = SPECIES_OGERPON_TEAL; item = ITEM_NONE; }
+    PARAMETRIZE { species = SPECIES_BLASTOISE; ogerpon = SPECIES_OGERPON_TEAL;        item = ITEM_NONE; }
     PARAMETRIZE { species = SPECIES_CHARIZARD; ogerpon = SPECIES_OGERPON_CORNERSTONE; item = ITEM_CORNERSTONE_MASK; }
-    PARAMETRIZE { species = SPECIES_CHARIZARD; ogerpon = SPECIES_OGERPON_WELLSPRING; item = ITEM_WELLSPRING_MASK; }
-    PARAMETRIZE { species = SPECIES_VENUSAUR; ogerpon = SPECIES_OGERPON_HEARTHFLAME;  item = ITEM_HEARTHFLAME_MASK; }
+    PARAMETRIZE { species = SPECIES_CHARIZARD; ogerpon = SPECIES_OGERPON_WELLSPRING;  item = ITEM_WELLSPRING_MASK; }
+    PARAMETRIZE { species = SPECIES_VENUSAUR;  ogerpon = SPECIES_OGERPON_HEARTHFLAME; item = ITEM_HEARTHFLAME_MASK; }
 
     GIVEN {
         PLAYER(ogerpon) { Item(item); }
