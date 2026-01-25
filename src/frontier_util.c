30db62d4b74017a0380f0f55d6a6103cb3d79dfb
grintoul <166724814+grintoul1@users.noreply.github.com>
2026-01-25 23:17:12 +0000

    Remove player party shiny bit setting in CreateFrontierBrainPokemon (#9022)

--- src/frontier_util.c
+++ src/frontier_util.c
@@ -2612,8 +2612,6 @@ void CreateFrontierBrainPokemon(void)
                 friendship = 0;
         }
         SetMonData(&gEnemyParty[monPartyId], MON_DATA_FRIENDSHIP, &friendship);
-        j = FALSE;
-        SetMonData(&gPlayerParty[MULTI_PARTY_SIZE + i], MON_DATA_IS_SHINY, &j);
         CalculateMonStats(&gEnemyParty[monPartyId]);
         monPartyId++;
     }
