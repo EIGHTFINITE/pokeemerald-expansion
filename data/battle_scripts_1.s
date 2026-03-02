264bda387ee87d39291e8e89ff21ecff18b6e4db
PhallenTree <168426989+PhallenTree@users.noreply.github.com>
2026-03-02 12:47:53 +0000

    Simplify Scale Shot effect activation

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -2548,10 +2548,6 @@ BattleScript_MultiHitPrintStrings::
 	waitmessage B_WAIT_TIME_LONG
 	return
 
-BattleScript_ScaleShot::
-	call BattleScript_MultiHitPrintStrings
-	goto BattleScript_DefDownSpeedUp
-
 BattleScript_EffectConversion::
 	attackcanceler
 	tryconversiontypechange BattleScript_ButItFailed
