182db69c9c3830bd05eb38a4c9ede6b91bd790e3
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2026-03-22 19:19:16 +0100

    Enable Sleep Clause

--- test/battle/gimmick/dynamax.c
+++ test/battle/gimmick/dynamax.c
@@ -1085,6 +1085,7 @@ DOUBLE_BATTLE_TEST("Dynamax: G-Max Befuddle paralyzes, poisons, or sleeps both o
     PARAMETRIZE { statusAnim = B_ANIM_STATUS_PSN; rng = STATUS1_POISON; }
     PARAMETRIZE { statusAnim = B_ANIM_STATUS_SLP; rng = STATUS1_SLEEP; }
     GIVEN {
+        WITH_CONFIG(B_SLEEP_CLAUSE, FALSE);
         ASSUME(MoveHasAdditionalEffect(MOVE_G_MAX_BEFUDDLE, MOVE_EFFECT_EFFECT_SPORE_SIDE));
         PLAYER(SPECIES_BUTTERFREE) { GigantamaxFactor(TRUE); }
         PLAYER(SPECIES_CATERPIE);
