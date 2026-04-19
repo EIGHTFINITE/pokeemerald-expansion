57de63deb641a1d59f502441847ae4cc4e139833
Martin Griffin <martinrgriffin@gmail.com>
2026-04-19 11:02:20 +0100

    Test Runner: Use mini_printf for '%S'.
    When the test system was first written mini_printf couldn't be
    guaranteed to be linked, but it has been guaranteed for a while now.

--- test/test_runner.c
+++ test/test_runner.c
@@ -820,8 +820,6 @@ static s32 MgbaPutchar_(s32 i, s32 c)
     return i;
 }
 
-extern const u8 gWireless_RSEtoASCIITable[];
-
 // Bare-bones, only supports plain %s, %S, and %d.
 static s32 MgbaVPrintf_(const char *fmt, va_list va)
 {
@@ -940,13 +938,9 @@ static s32 MgbaVPrintf_(const char *fmt, va_list va)
                 }
                 else
                 {
+                    extern char mini_pchar_decode(u8);
                     while ((c = *pokeS++) != EOS)
-                    {
-                        if ((c = gWireless_RSEtoASCIITable[c]) != '\0')
-                            i = MgbaPutchar_(i, c);
-                        else
-                            i = MgbaPutchar_(i, '?');
-                    }
+                        i = MgbaPutchar_(i, mini_pchar_decode(c));
                 }
                 break;
             }
