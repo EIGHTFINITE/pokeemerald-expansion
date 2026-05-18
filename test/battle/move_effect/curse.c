3bf7c9f74ce98ff529e25cd6ce99a35fd3587569
hedara90 <90hedara@gmail.com>
2026-05-18 09:49:49 +0200

    Add test for Curse + Baton Pass interaction (#10000)

--- test/battle/move_effect/curse.c
+++ test/battle/move_effect/curse.c
@@ -26,6 +26,7 @@ SINGLE_BATTLE_TEST("Curse lowers Speed, raises Attack, and raises Defense when u
 SINGLE_BATTLE_TEST("Curse cuts the user's HP in half when used by Ghost-types")
 {
     GIVEN {
+        ASSUME(GetSpeciesType(SPECIES_MISDREAVUS, 0) == TYPE_GHOST || GetSpeciesType(SPECIES_MISDREAVUS, 0) == TYPE_GHOST);
         PLAYER(SPECIES_MISDREAVUS);
         OPPONENT(SPECIES_WOBBUFFET);
     } WHEN {
@@ -73,4 +74,21 @@ SINGLE_BATTLE_TEST("Curse applies to the opponent if user is afflicted by Trick-
     }
 }
 
-TO_DO_BATTLE_TEST("Baton Pass passes Cursed status");
+SINGLE_BATTLE_TEST("Baton Pass passes Cursed status")
+{
+    GIVEN {
+        ASSUME(GetSpeciesType(SPECIES_MISDREAVUS, 0) == TYPE_GHOST || GetSpeciesType(SPECIES_MISDREAVUS, 0) == TYPE_GHOST);
+        ASSUME(GetMoveEffect(MOVE_BATON_PASS) == EFFECT_BATON_PASS);
+        PLAYER(SPECIES_WOBBUFFET);
+        PLAYER(SPECIES_WYNAUT);
+        OPPONENT(SPECIES_MISDREAVUS);
+    } WHEN {
+        TURN { MOVE(opponent, MOVE_CURSE); MOVE(player, MOVE_BATON_PASS); SEND_OUT(player, 1); }
+    } SCENE {
+        s32 playerMaxHP = GetMonData(&PLAYER_PARTY[1], MON_DATA_MAX_HP);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_CURSE, opponent);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_BATON_PASS, player);
+        SEND_IN_MESSAGE("Wynaut");
+        HP_BAR(player, damage: playerMaxHP / 4);
+    }
+}
