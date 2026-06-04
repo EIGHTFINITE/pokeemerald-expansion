2230b0d5420902a77964591644fa78283c9b669f
John Cooper McDonald <mario.pikmin.zelda@gmail.com>
2026-06-04 15:10:55 -0500

    Add GEN_CHAMPIONS + move data (#10058)

--- include/config/general.h
+++ include/config/general.h
@@ -68,7 +68,8 @@
 #define GEN_7 6
 #define GEN_8 7
 #define GEN_9 8
-#define GEN_COUNT 9
+#define GEN_CHAMPIONS GEN_9 + 1
+#define GEN_COUNT GEN_CHAMPIONS
 // Changing GEN_LATEST's value to a different Generation will change every default setting that uses it at once.
 #define GEN_LATEST GEN_9
 
