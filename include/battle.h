12de80e9c09a7e89daf9ea8ff8d25a82c61c3c4d
Cle-bit <172129134+Cle-bit@users.noreply.github.com>
2026-07-26 22:41:53 +0800

    Fix Rage Fist Hit counter overflow (#10514)

--- include/battle.h
+++ include/battle.h
@@ -536,11 +536,11 @@ struct PartyState
     u32 battleBondBoost:1;
     u32 transformZeroToHero:1;
     u32 supersweetSyrup:1;
-    u32 timesGotHit:5;
+    u32 timesGotHit:8;
     u32 changedSpecies:11; // For forms when multiple mons can change into the same Pokémon.
     u32 sentOut:1;
     u32 isKnockedOff:1;
-    u32 padding:8;
+    u32 padding:5;
     u16 usedHeldItem;
 };
 
