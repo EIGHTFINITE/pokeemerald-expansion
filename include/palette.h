4a39c64fe4f208285fcd9d87eec760aff8b35778
FosterProgramming <jamiefosterandco@gmail.com>
2026-08-04 12:54:22 +0200

    Rework simultaneous palette fade (#10573)
    Co-authored-by: Bassoonian <iasperbassoonian@gmail.com>

--- include/palette.h
+++ include/palette.h
@@ -77,7 +77,8 @@ struct PaletteFadeControl
     bool32 softwareFadeFinishing:1;
     bool32 objPaletteToggle:1;
     u32 deltaY:4; // rate of change of blend coefficient
-    u32 padding:15;
+    u32 simultaneousFade:1; // instead of alternating between fading sptite and bg, fade both simultaneously (to avoid visual inconsistencies in rare scenarios)
+    u32 padding:14;
 };
 
 extern const struct BlendSettings gTimeOfDayBlend[];
