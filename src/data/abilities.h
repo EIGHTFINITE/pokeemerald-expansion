e98329ee4b91109360bee37a9d3bfa9e7ac656f5
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2026-08-17 19:49:17 +0200

    Both versions of Snow Warning
    Renaming the Gen8 version to Hailstorm.

--- src/data/abilities.h
+++ src/data/abilities.h
@@ -923,11 +923,7 @@ const struct AbilityInfo gAbilitiesInfo[ABILITIES_COUNT] =
     [ABILITY_SNOW_WARNING] =
     {
         .name = _("Snow Warning"),
-    #if B_SNOW_WARNING >= GEN_9
         .description = COMPOUND_STRING("Summons snow in battle."),
-    #else
-        .description = COMPOUND_STRING("Summons hail in battle."),
-    #endif
         .aiRating = 8,
     },
 
@@ -2549,5 +2545,12 @@ const struct AbilityInfo gAbilitiesInfo[ABILITIES_COUNT] =
     {
         .name = _("Spicy Spray"),
         .description = COMPOUND_STRING("Burns the foe when damaged."),
+    },
+
+    [ABILITY_SNOW_WARNING_GEN8] =
+    {
+        .name = _("Hailstorm"),
+        .description = COMPOUND_STRING("Summons hail in battle."),
+        .aiRating = 8,
     },
 };
