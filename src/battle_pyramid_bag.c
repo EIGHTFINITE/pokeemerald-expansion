pret/pokeemerald

Replace item define list with enum

src/battle_pyramid_bag.c
@@ -942,7 +942,7 @@ static void OpenContextMenu(u8 taskId)
         gPyramidBagMenu->menuActionsCount = ARRAY_COUNT(sMenuActionIds_Field);
         break;
     case PYRAMIDBAG_LOC_BATTLE:
-        if (ItemId_GetBattleUsage(gSpecialVar_ItemId))
+        if (ItemId_GetBattleUsage(gSpecialVar_ItemId) != ITEM_B_USE_NONE)
         {
             gPyramidBagMenu->menuActionIds = sMenuActionIds_Battle;
             gPyramidBagMenu->menuActionsCount = ARRAY_COUNT(sMenuActionIds_Battle);

src/item_menu.c
@@ -1533,7 +1533,7 @@ static void OpenContextMenu(u8 taskId)
     {
     case ITEMMENULOCATION_BATTLE:
     case ITEMMENULOCATION_WALLY:
-        if (ItemId_GetBattleUsage(gSpecialVar_ItemId))
+        if (ItemId_GetBattleUsage(gSpecialVar_ItemId) != ITEM_B_USE_NONE)
         {
             gBagMenu->contextMenuItemsPtr = sContextMenuItems_BattleUse;
             gBagMenu->contextMenuNumItems = ARRAY_COUNT(sContextMenuItems_BattleUse);

EIGHTFINITE/pokeemerald-expansion

src/battle_pyramid_bag.c
@@ -941,7 +941,7 @@ static void OpenContextMenu(u8 taskId)
         gPyramidBagMenu->menuActionsCount = ARRAY_COUNT(sMenuActionIds_Field);
         break;
     case PYRAMIDBAG_LOC_BATTLE:
-        if (GetItemBattleUsage(gSpecialVar_ItemId))
+        if (GetItemBattleUsage(gSpecialVar_ItemId) != ITEM_B_USE_NONE)
         {
             gPyramidBagMenu->menuActionIds = sMenuActionIds_Battle;
             gPyramidBagMenu->menuActionsCount = ARRAY_COUNT(sMenuActionIds_Battle);

src/item_menu.c
@@ -1532,7 +1532,7 @@ static void OpenContextMenu(u8 taskId)
     {
     case ITEMMENULOCATION_BATTLE:
     case ITEMMENULOCATION_WALLY:
-        if (GetItemBattleUsage(gSpecialVar_ItemId))
+        if (GetItemBattleUsage(gSpecialVar_ItemId) != ITEM_B_USE_NONE)
         {
             gBagMenu->contextMenuItemsPtr = sContextMenuItems_BattleUse;
             gBagMenu->contextMenuNumItems = ARRAY_COUNT(sContextMenuItems_BattleUse);